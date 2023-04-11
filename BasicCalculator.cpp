3290cdbd23c0448aa9e3ac0e3b77a45c83472be2
김익현/연구원/Audio Unit(ikhyeon.kim@lge.com) <ikhyeon.kim@lge.com>
2023-04-11 10:07:47 +0900

    init Basic Calculator

---
+++ BasicCalculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define MAX (20)
+int N;
+int M[MAX + 10];
+char op[MAX + 10];
+
+void InputData(){
+	cin >> N;
+	cin >> M[0];
+	for(int i=1; i<N; i++){
+		cin >> op[i] >> M[i];
+	}
+}
+
+int main(){
+	int ans = -1;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	InputData();//입력
+
+	//여기서부터 작성
+
+	cout << ans << "\n";
+
+	return 0;
+}
